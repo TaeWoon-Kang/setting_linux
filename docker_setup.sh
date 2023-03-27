2959c748d8f83fdf32f0823508c30d9748d021b8
TaeWoon-Kang <taewoon1374@gmail.com>
2023-03-27 14:46:27 +0900

    corelli

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,8 +1,11 @@
 apt-get update
 apt-get upgrade
-apt-get install -y vim tmux wget curl lsb-release git tmux python3 python3-pip cmake build-essential apt-transport-https software-properties-common
 
+apt-get install -y tmux wget curl lsb-release git tmux python3 python3-pip cmake build-essential apt-transport-https software-properties-common
+apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo add-apt-repository ppa:jonathonf/vim
+apt-get install -y vim
 cp .vimrc ~/.vim/
 vim +PluginInstall +qall
 
