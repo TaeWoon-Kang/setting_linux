1e0cb4fe567b20f2c27e3025c683d3708851ed47
TaeWoon-Kang <taewoon1374@gmail.com>
2023-06-10 15:44:49 +0900

    june_10

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,6 +1,7 @@
 apt-get update
 apt-get upgrade
 
+apt install sudo
 apt-get install -y tmux wget curl lsb-release git tmux python3 python3-pip cmake build-essential apt-transport-https software-properties-common
 apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
